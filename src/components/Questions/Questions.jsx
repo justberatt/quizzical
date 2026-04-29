c050d8c0934eb873c8027808586d9211d574b5c3
Berat Sakipi <berat.sakipi@gmail.com>
2026-04-29 23:53:51 +0200

    Add logic for fetching the question from the Open Trivia Database API, also add the 'Check Answers' button.

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,7 +1,29 @@
-const QuestionsPage = () => {
-    return (
-        <h1>This is the questions page!</h1>
-    )
-}
+import styles from "./Questions.module.css";
+import { useState, useEffect } from "react";
 
-export default QuestionsPage
+const QuestionsPage = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple",
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        data.results.forEach((question) => console.log(question.question));
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading ...</p>;
+  return (
+    <main className={styles.questionsPageMainContainer}>
+      {/* <ul>{data.}</ul> */}
+      <button className={styles.checkAnswersBtn}>Check Anwers</button>
+    </main>
+  );
+};
+
+export default QuestionsPage;
